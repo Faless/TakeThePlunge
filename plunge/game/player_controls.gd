14dee23de4b93f1f0f4cd63afdcae994ccc1a0c6
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-11-12 00:26:14 +0100

    Action names.

--- plunge/game/player_controls.gd
+++ plunge/game/player_controls.gd
@@ -6,21 +6,24 @@ class_name PlayerControls
 var player := 1 :
 	set(value):
 		player = value
-		if sync:
-			sync.set_multiplayer_authority(value)
-
+		if sync: sync.set_multiplayer_authority(value)
 
 var input_dir := Vector2()
 var jumping := false
+
+
+func _ready():
+	sync.set_multiplayer_authority(player)
+
 
 func _physics_process(delta):
 	if multiplayer.get_unique_id() != player:
 		return
 
 	# Get the input direction and handle the movement/deceleration.
-	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_dir = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_backward")
 	jumping = false
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("action_jump"):
 		jump.rpc()
 
 @rpc(call_local)
