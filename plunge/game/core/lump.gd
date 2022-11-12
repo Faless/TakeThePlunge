bf198ed5be9374b8e17ed01cf0520ba8aa4bc2d4
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-11-12 13:38:16 +0100

    Add lump lifetime.

---
+++ plunge/game/core/lump.gd
@@ -0,0 +1,13 @@
+extends RigidBody3D
+
+var lifetime := 5.0
+
+
+func _ready():
+	if not is_zero_approx(lifetime) and multiplayer.is_server():
+		$Lifetime.wait_time = lifetime
+		$Lifetime.start()
+
+
+func _on_lifetime_timeout():
+	queue_free()
