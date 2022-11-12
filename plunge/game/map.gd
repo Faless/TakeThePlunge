45b004657d98c15008087b32a17378eac721a507
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-11-12 02:06:09 +0100

    despawn.

--- plunge/game/map.gd
+++ plunge/game/map.gd
@@ -1,13 +1,17 @@
 extends Node3D
 
-@onready var player_spawer := $MultiplayerSpawner as MultiplayerSpawner
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	if multiplayer.is_server():
 		spawn_player(1)
 		multiplayer.peer_connected.connect(spawn_player)
+		multiplayer.peer_disconnected.connect(despawn_player)
 
 
 func spawn_player(id: int):
-	player_spawer.spawn(id)
+	$MultiplayerSpawner.spawn(id)
+
+
+func despawn_player(id: int):
+	for p in $Players.get_children():
+		if p.player == id:
+			p.queue_free()
