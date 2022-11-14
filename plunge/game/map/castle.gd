f3ff5db09824e3cece2a119187353923b48e2baa
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-11-14 22:10:09 +0100

    Castle rotation configuration.

--- plunge/game/map/castle.gd
+++ plunge/game/map/castle.gd
@@ -4,6 +4,8 @@ signal plunged(lump_transform: Transform3D, lump_linear_velocity: Vector3)
 
 @export var rotation_speed := .01
 @export var plunge_fuzzy := 1.0
+@export var shooting := false
+@export var rotating := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,8 +13,12 @@ func _ready():
 		plunger.plunged.connect(_plunged)
 
 func _plunged(lump_transform: Transform3D, lump_linear_velocity: Vector3):
+	if not shooting:
+		return
 	var fuzzy = Vector3.ONE * (randf() - .5) * plunge_fuzzy
 	plunged.emit(lump_transform, lump_linear_velocity + fuzzy)
 
 func _physics_process(delta):
+	if not rotating:
+		return
 	rotate(Vector3.UP, rotation_speed)
